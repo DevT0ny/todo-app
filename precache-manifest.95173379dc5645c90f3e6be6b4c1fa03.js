550957dc333b978d9d4990ba67e8e99a59ca8624
Ajith-stark <ajith.kum.12342@gmail.com>
2020-07-09 16:47:49 +0530

    Updates

---
+++ precache-manifest.95173379dc5645c90f3e6be6b4c1fa03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1e74c76abd11d3b2d42d37fc3ef80f",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "a686a8bae79bf955f2f4",
+    "url": "/todo-app/static/css/main.1a68396b.chunk.css"
+  },
+  {
+    "revision": "5e0dfd9c26990e80d608",
+    "url": "/todo-app/static/js/2.ca71bcac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.ca71bcac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a686a8bae79bf955f2f4",
+    "url": "/todo-app/static/js/main.07d15947.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
